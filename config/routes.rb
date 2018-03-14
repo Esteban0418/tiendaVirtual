fc708241698b12516f6f0babf2487c581c3abdbc
Esteban Echeverry <Esteban-0418@hotmail.com>
2018-03-14 12:02:57 -0500

    Cambio de ruta para el home y se agrego precio al formulario de creacion de un producto

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :inventarios
   resources :productos
   resources :users
+
+  root 'productos#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
