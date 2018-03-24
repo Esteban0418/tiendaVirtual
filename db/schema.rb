153680f9805783166676f3630814c3b8cbe82711
Felipe <ymvelasquezr@uqvirtual.edu.co>
2018-03-24 13:08:09 -0500

    se c reo una migracion para que en precio se pdiera poner valores grandes y se agrego la columna de precio al index y al show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180322223337) do
+ActiveRecord::Schema.define(version: 20180324174453) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 20180322223337) do
     t.string "dimenciones"
     t.string "color"
     t.integer "peso"
-    t.integer "precio"
+    t.integer "precio", limit: 8
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "cantidad"
