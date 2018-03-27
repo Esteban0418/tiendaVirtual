7f2f8f6cfea2e5c0987143894c6bfc2b83283fd5
Felipe <ymvelasquezr@uqvirtual.edu.co>
2018-03-27 15:34:22 -0500

    redireccion de paginas

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-if Producto.present?
-	
-Inventario.create(cantidad: 1, precio: 0)
 
-end
 
 User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
-
+Role.create!(name: 'administrador')
+Role.create!(name: 'cliente')
 
