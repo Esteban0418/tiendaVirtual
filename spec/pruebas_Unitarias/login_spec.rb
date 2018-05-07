8093cbbb96a5724eb56636418a996cd4ab12b2b5
Felipe <ymvelasquezr@uqvirtual.edu.co>
2018-05-07 14:19:16 -0500

    prueba unitaria del registro de usuario

--- spec/pruebas_Unitarias/login_spec.rb
+++ spec/pruebas_Unitarias/login_spec.rb
@@ -10,11 +10,4 @@ RSpec.feature "pruebas unitarias del login" do
 		#capibara
 		#expect(page) 
 	end
-	it "cambiarcontraseña" do
-	visit root_path
-	click_on "Iniciar sesion"
-     lick_on "Forgot your password?"
-    fill_in "user[email]", with: "yessika@hotmail.com"
-	click_button('Send me reset password instructions')
-	
 end
---
+++ spec/pruebas_Unitarias/registrar_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+RSpec.feature "pruebas unitarias del registro" do 
+
+	it "registro correcto" do 
+		visit root_path
+		click_on "Registrarse"
+		fill_in "user[documento]", with: "123"
+		fill_in "user[NIT]", with: "12234233"
+		fill_in "user[telefono]", with: "71232123"
+		fill_in "user[nombre]", with: "meliza"
+		fill_in "user[direccion]", with: "30-23-23"
+		fill_in "user[celular]", with: "32131231221"
+		fill_in "user[ciudad]", with: "armenia"
+		fill_in "user[password]", with: "123456789"
+		fill_in "user[profesion]", with: "ing. de sistemas"
+		fill_in "user[email]", with: "yessikameliza@uqvirtual.edu.co"
+		click_button('Create User')
+		#capibara
+		#expect(page) 
+	end
+end
