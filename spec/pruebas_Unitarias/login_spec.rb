a30ec404b7410637e52b594fec3c9b04a913e662
Felipe <ymvelasquezr@uqvirtual.edu.co>
2018-05-24 16:30:08 -0500

    front login

--- spec/pruebas_Unitarias/login_spec.rb
+++ spec/pruebas_Unitarias/login_spec.rb
@@ -4,10 +4,10 @@ RSpec.feature "pruebas unitarias del login" do
 	it "login correcto" do 
 		visit root_path
 		click_on "Iniciar sesion"
-		fill_in "user[email]", with: "yessika@hotmail.com"
-		fill_in "user_password", with: "123456789"
+		fill_in "user[email]", with: "yessika@"
+		fill_in "user_password", with: "1234567890"
 		click_button('Log in')
-		
+		page.has_content?('Productos disponibles')
 		#capibara
 		#expect(page) 
 	end
@@ -17,6 +17,7 @@ RSpec.feature "pruebas unitarias del login" do
 		fill_in "user[email]", with: "yessika@"
 		fill_in "user_password", with: "123456789"
 		click_button('Log in')
+		page.has_content?('Log in')
 		#capibara
 		#expect(page) 
 	end
