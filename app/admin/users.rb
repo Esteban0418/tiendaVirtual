f7fcd309d2b37243155213ba59aab7c8bb632eed
Esteban Echeverry <Esteban-0418@hotmail.com>
2018-05-25 01:53:04 -0500

    mejoras sencillas

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,6 +1,26 @@
 ActiveAdmin.register User do
   permit_params :email, :password, :password_confirmation
 
+  controller do
+    # This code is evaluated within the controller class
+
+    def destroy
+      @user = User.find(params[:id])
+      if @user.reportes.empty?
+        @user.destroy
+        respond_to do |format|
+          format.html { redirect_to users_url, notice: 'Usuario eliminado de forma exitosa.' }
+          format.json { head :no_content }
+        end
+      else
+        respond_to do |format|
+          format.html { redirect_to users_url, notice: 'El usuario ha realizado compras y no puede ser eliminado.' }
+          format.json { head :no_content }
+        end
+      end
+    end
+  end
+  
   index do
     selectable_column
     id_column
