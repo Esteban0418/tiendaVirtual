acbcef1b1bcf3654be2861f3724417a1682af56f
Felipe <ymvelasquezr@uqvirtual.edu.co>
2018-05-25 01:10:45 -0500

    pruebas de agregar productos al carrito

--- spec/pruebas_Unitarias/add_producto_carrito_spec.rb
+++ spec/pruebas_Unitarias/add_producto_carrito_spec.rb
@@ -16,6 +16,24 @@ RSpec.feature "pruebas unitarias del registro de un producto" do
 		fill_in "user[email]", with: "yessikameliza@hotmail.com"
 		click_button('Create User')
 		page.has_content?('32131231221')
+		visit ('/admin')
+		fill_in "user[email]", with: "admin@example.com"
+		fill_in "user[password]", with: "password"
+		click_button('Log in')
+		click_on "Productos"
+		click_on "New Producto"
+		fill_in "producto[nombre]", with: "dell"
+		fill_in "producto[descripcion]", with: "computador portatil"
+		fill_in "producto[tipo]", with: "computador"
+		fill_in "producto[material]", with: "hierro	"
+		fill_in "producto[precio]", with: "1234423"
+		fill_in "producto[dimenciones]", with: "30-23-23"
+		fill_in "producto[color]", with: "#000000"
+		fill_in "producto[peso]", with: "3"
+		fill_in "producto[cantidad]", with: "12"
+		click_button('Create Producto')
+		page.has_content?('dell')
+	    visit root_path
 		click_on "Iniciar sesion"
 		fill_in "user[email]", with: "yessika@hotmail.com"
 		fill_in "user_password", with: "1234567890"
@@ -23,6 +41,9 @@ RSpec.feature "pruebas unitarias del registro de un producto" do
 		page.has_content?('Productos disponibles')
 		click_on "Productos"
 		click_on "Carrito"
+		visit carritos_path
+		page.has_content?('1')
+
 		#capibara
 		#expect(page) 
 	end
